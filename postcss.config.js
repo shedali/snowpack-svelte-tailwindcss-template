e57e635879913d71a9e8ce45523ea4beb29a6c2b
Franz <shedali@gmail.com>
2020-08-30 21:14:31 +0100

    init

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./index.html", "./src/**/*.js"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+import {types as t} from 'https://cdn.pika.dev/mobx-state-tree@^3.15.0';
+
+	const TestModel = t.model({
+		name: t.string
+	})
+
+	const m = TestModel.create({
+		name: "test"
+	})
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="output.css">
+</svelte:head>
+
+<p class="text-3xl border">
+	nice one mate {m.name}
+</p>
+
+
+
